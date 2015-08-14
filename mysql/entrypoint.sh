e1b0f8ccc28b5a9700afa5c0bdfddf47aff0905f
linbo <vkyii@foxmail.com>
2015-08-15 07:13:14 +0800

    *Add mysql

---
+++ mysql/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+INITFILE=/tmp/init.sql
+touch $INITFILE
+
+if [ -n $ROOT_PASSWORD ]; then
+  echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$ROOT_PASSWORD'" >> $INITFILE
+fi
+
+if [ -n "$USERNAME" ]; then
+  echo "GRANT ALL ON *.* TO '$USERNAME'@'%' IDENTIFIED BY '$PASSWORD'" >> $INITFILE
+fi
+
+mysqld_safe --init-file=$INITFILE
