6c7c4c85538557aef74cf631ebe050f5cab73bb9
linbo <vkyii@foxmail.com>
2015-08-19 15:32:09 +0800

    *Debug

--- ejbca/test/script/build-ejbca.sh
+++ ejbca/test/script/build-ejbca.sh
@@ -9,6 +9,27 @@ sed -i 's/jboss.bind.address.management:127.0.0.1/jboss.bind.address.management:
 $APPSRV_HOME/bin/standalone.sh &
 
 #
+# 等待Mysql启动
+#
+while [[ `netstat -an | grep 3306 | wc -l` == 0 ]];
+do
+        echo "Wating for Mysql start up"
+        sleep 1;
+done
+echo "Mysql is UP!"
+
+#
+# 等待JBoss启动
+#
+while [[ `netstat -an | grep 8080 | wc -l` == 0 ]];
+do
+        echo "Wating for JBoss start up"
+        sleep 1;
+done
+echo "JBoss is UP!"
+ 
+
+#
 # register driver
 # FIX ME!
 # wait for jboss startup end
@@ -22,6 +43,7 @@ $APPSRV_HOME/bin/jboss-cli.sh -c --command=':shutdown(restart=true)'
 # 仅保留上一步mysql的
 # 计划用python读写xml
 #
+# 这一步在公司的服务器机器上可以省略
 
 
 #
@@ -36,9 +58,12 @@ cd $EJBCA_HOME
 
 # The build uses non-default handler in some cases.  Remove these so we
 # can run fully non-interactively build with PropertyFileInputHandler
-sed -i '/SecureInputHandler/d' bin/cli.xml
+#sed -i '/SecureInputHandler/d' bin/cli.xml
 
 $ANT_HOME/bin/ant deploy
+
+# ant install这步需要jboss重启,可以提前做,不要问为什么,手误试出来的 
+$APPSRV_HOME/bin/jboss-cli.sh -c --command=':shutdown(restart=true)'
 $ANT_HOME/bin/ant install
 
 # 处理一个错误
