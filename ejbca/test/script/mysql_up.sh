e5ebdd0dcbf1040ca7b408ad7ba448df38459d5b
linbo <vkyii@foxmail.com>
2015-08-19 08:48:53 +0800

    *Debug

--- ejbca/test/script/mysql_up.sh
+++ ejbca/test/script/mysql_up.sh
@@ -4,8 +4,6 @@ INITFILE=/tmp/init.sql
 touch $INITFILE
 
 
-echo "create database ejbca;" >> $INITFILE
-
 if [ -n $ROOT_PASSWORD ]; then
   echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$ROOT_PASSWORD'" >> $INITFILE
 fi
@@ -14,13 +12,15 @@ if [ -n "$USERNAME" ]; then
   echo "GRANT ALL ON *.* TO '$USERNAME'@'%' IDENTIFIED BY '$PASSWORD'" >> $INITFILE
 fi
 
-mysqld_safe --init-file=$INITFILE
 
 # init ejbca db
-# cat > /tmp/ejbca.sql <<EOF
-# create database ejbca;
-# grant all privileges on ejbca.* to 'ejbca'@'localhost' identified by 'ejbca';
-# flush privileges;
-# EOF
+cat > INITFILE <<EOF
+create database ejbca;
+grant all privileges on ejbca.* to 'ejbca'@'localhost' identified by 'ejbca';
+flush privileges;
+EOF
+
+mysqld_safe --init-file=$INITFILE
+mysql_secure_installation
 
 # mysql -u root -e "/tmp/ejbca.sql"
